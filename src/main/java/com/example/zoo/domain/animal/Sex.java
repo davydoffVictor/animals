dfebbba6857f11e2d3599f8f2e9bf1bc07c04f56
Victor Davydoff <davictor@yandex.ru>
2025-10-08 19:57:22 +0300

    1. DB schema with necessary tables created 2. Created entities for User and Animal

---
+++ src/main/java/com/example/zoo/domain/animal/Sex.java
@@ -0,0 +1,5 @@
+package com.example.zoo.domain.animal;
+
+public enum Sex {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/example/zoo/domain/animal/Type.java
@@ -0,0 +1,5 @@
+package com.example.zoo.domain.animal;
+
+public enum Type {
+    DOG, CAT, RAT, MOUSE, PIG
+}
