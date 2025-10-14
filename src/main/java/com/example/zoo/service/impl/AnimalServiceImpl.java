986ab4b863cf85c9f7fddcf8d990c382b2f42b38
Victor Davydoff <davictor@yandex.ru>
2025-10-14 21:13:32 +0300

    1. Added @Transactional annotations in Animal Service (forgot to do it earlier)

--- src/main/java/com/example/zoo/service/impl/AnimalServiceImpl.java
+++ src/main/java/com/example/zoo/service/impl/AnimalServiceImpl.java
@@ -5,7 +5,9 @@ import com.example.zoo.repository.AnimalRepository;
 import com.example.zoo.service.AnimalService;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -17,17 +19,26 @@ public class AnimalServiceImpl implements AnimalService {
     private final AnimalRepository animalRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Animal getById(Long id) {
         Optional<Animal> animal =  animalRepository.findById(id);
         return animal.orElseThrow(() -> new NoSuchElementException("Animal not found id = " + id)); //TODO exception handler
     }
 
     @Override
+    @Transactional(readOnly = true)
+    public List<Animal> getAllByUserId(Long userId) {
+        return animalRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    @Transactional
     public void delete(Long id) {
         animalRepository.deleteById(id);
     }
 
     @Override
+    @Transactional
     public Animal update(Animal animal) {
         return animalRepository.save(animal);
     }
